f8699195e8598824099b51978ff0d358812611fa
Leonel <thushakk4@gmail.com>
2021-06-21 02:36:16 -0430

    add logo

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,10 +6,16 @@ export default {
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
-      { name: 'theme-color', content: '#0a0b35' }
+      { name: 'theme-color', content: '#0a0b35' },
+      { hid: 'description', name: 'description', content: 'Desarollador Front-end con experiencia en una de las últimas tecnologías usadas actualmente.' },
+      { property: 'og:https://augustodiaz.netlify.app/', content: 'Front-end Developer | Augusto Díaz' },
+      { property: 'og:title', content: 'Portafolio' },
+      { property: 'og:description', content: 'Desarollador Front-end con experiencia en una de las últimas tecnologías usadas actualmente.' },
+      { property: 'og:image', content: 'https://i.ibb.co/XXRLCQz/logo24.png' },
+      { property: 'type', content: 'blog' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'icon', type: 'image/png', href: '/logo24.png' },
       { rel: 'stylesheet', type: 'text/css', href: 'https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css' },
       { rel: 'preconnect', href: 'https://fonts.gstatic.com' },
       { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Fira+Sans:wght@300&family=Inconsolata:wght@300&display=swap' }
